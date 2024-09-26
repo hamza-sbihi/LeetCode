e71db8a6894c111ada2c8e73d349d21a6aa5015a
hamza sbihi <shamza1235@gmail.com>
2024-09-26 22:46:37 +0200

    tree

---
+++ Trees/BFS max depth.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        Queue<TreeNode> treeQ = new LinkedList<>();
+        int depth=0;
+        treeQ.offer(root);
+        while(!treeQ.isEmpty()){
+            int len=treeQ.size();
+            for(int i=0;i<len;i++){
+                TreeNode current = treeQ.poll();
+                if(current.right!=null) treeQ.offer(current.right);
+                if(current.left!=null) treeQ.offer(current.left);
+            }
+            depth++;
+        }
+        return depth;
+        
+    }
+}
